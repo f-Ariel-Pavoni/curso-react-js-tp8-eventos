281901e02febeb31330d96988666b017258c407e
Ariel Pavoni <flvariel@gmail.com>
2026-07-24 14:52:32 -0300

    ADD: formularios y funciones

--- src/components/FormularioControlado/FormularioControlado.jsx
+++ src/components/FormularioControlado/FormularioControlado.jsx
@@ -1,4 +1,56 @@
+import { useState } from "react";
+
 const FormularioControlado = () => {
+  const [formulario, setFormulario] = useState({ nombre: "", email: "" });
+  const [btnActivo, setBtnActivo] = useState(false);
+  const [mensaje, setMensaje] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formulario);
+
+    //Reset de formulario al enviar
+    setFormulario({
+      nombre: "",
+      email: "",
+    });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormulario({
+      ...formulario,
+      [name]: value,
+    });
+  };
+
+  const handleFocus = (e) => {
+    //estado de la interfaz - muestro mensaje en la interfaz
+    setMensaje(`El campo ${e.target.name} está activo`);
+    console.log(`El campo ${e.target.name} está activo`);
+  };
+
+  const handleBlur = (e) => {
+    //estado de la interfaz - muestro mensaje en la interfaz
+    setMensaje(`El campo ${e.target.name} perdió el foco.`);
+    console.log(`El campo ${e.target.name} perdió el foco.`);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter")
+      console.log(`Se presiono enter sobre el cammpo ${e.target.name}.`);
+  };
+
+  const handleMouseEnter = (e) => {
+    setBtnActivo(true);
+    console.log(`Se posó sobre ${e.target.textContent}.`);
+  };
+
+  const handleMouseLeave = (e) => {
+    setBtnActivo(false);
+    console.log(`Abandonó ${e.target.textContent}.`);
+  };
+
   return (
     <div className="card shadow">
       <div className="card-body">
@@ -6,7 +58,7 @@ const FormularioControlado = () => {
           Formulario Eventos Controlados
         </h3>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="nombre" className="form-label">
               Nombre:
@@ -15,11 +67,16 @@ const FormularioControlado = () => {
             <input
               type="text"
               id="nombre"
+              value={formulario.nombre}
               name="nombre"
               className="form-control"
               placeholder="Tu nombre"
               autoComplete="name"
               required
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              onKeyDown={handleKeyDown}
             />
           </div>
 
@@ -31,20 +88,30 @@ const FormularioControlado = () => {
             <input
               type="email"
               id="email"
+              value={formulario.email}
               name="email"
               className="form-control"
               placeholder="tu@email.com"
               autoComplete="email"
               required
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
             />
           </div>
 
           <div className="text-center">
-            <button type="submit" className="btn btn-primary">
+            <button
+              type="submit"
+              className={btnActivo ? "btn btn-success" : "btn btn-primary"}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
               Enviar
             </button>
           </div>
         </form>
+        {mensaje && <div className="alert alert-info mt-3">{mensaje}</div>}
       </div>
     </div>
   );
